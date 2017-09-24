e008cb7e02a6814491823c17c6ee3c84202a0c97
Zhao-Lin <lynchzhao@gmail.com>
2017-09-23 22:18:58 -0400

    Update 4-5.cpp
    This original is incorrect.

--- Accelerated C++/chapter04/4-5.cpp
+++ Accelerated C++/chapter04/4-5.cpp
@@ -1,48 +1,54 @@
+#include <algorithm>
+#include <iomanip>
 #include <iostream>
+#include <stdexcept>
 #include <string>
 #include <vector>
 
 using namespace std;
 
-istream& read_words(istream& in, vector<string>& words) {
-  if (in) {
-    words.clear();
-    string word;
-    
-    while (in >> word)
-      words.push_back(word);
-    
-    in.clear();
-  }
-  
-  return in;
+int main()
+{
+	vector <string> strs;
+	// to store the unique words that have been entered;
+	vector <string> uniq;
+	vector<int> wdct;
+
+	
+
+	typedef vector<string>::size_type st_sz;
+
+
+	string x;
+	while (cin >> x){
+		strs.push_back(x);
+	}
+
+	for (st_sz j = 0; j != strs.size(); ++j){
+		int count_temp = 1;
+		// suppose uniq has the the element i
+		int flag = 1;
+		for (st_sz i = 0; i != uniq.size(); ++i){
+
+			if (strs[j].compare(uniq[i])==0){
+				wdct[i]++;
+				flag = 0;
+			}
+		}
+		// haven't been add to the "uniq" vector
+		if (flag == 1){
+			uniq.push_back(strs[j]);
+			wdct.push_back(1);
+		}
+	}
+
+	cout << "You entered " << strs.size() << " words." << endl;
+
+	for (st_sz i = 0; i != uniq.size(); ++i){
+		cout << uniq[i] << string(10 - uniq[i].size(), ' ') << wdct[i]<<endl;
+	}
+	
+	system("pause");
+	return 0;
 }
 
-int main() {
-  vector<string> words;
-
-  read_words(cin, words);
-
-  cout << "Num of words: " << words.size() << endl;
-
-  sort(words.begin(), words.end());
-
-  string prev_word = "";
-  int count = 0;
-
-  for (vector<string>::size_type i = 0; i < words.size(); ++i) {
-    if (words[i] != prev_word) {
-      if (prev_word != "")
-	cout << prev_word << " appeared " << count << " times" << endl;
-
-      prev_word = words[i];
-      count = 1;
-    }
-    else
-      ++count;
-  }
-
-  cout << prev_word << " appeared " << count << " times" << endl;
-
-  return 0;
-}
